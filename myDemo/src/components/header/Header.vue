580a97a03de2c917cbf0b6d9124d155a72f05df0
@chenqingbiao <cqb17364045247@163.com>
2020-06-09 17:08:50 +0800

    头部导航

--- myDemo/src/components/header/Header.vue
+++ myDemo/src/components/header/Header.vue
@@ -19,35 +19,7 @@
                     </p>
                   </li>
                 </ul>
-              </div>
-            </div>
-            <div class="login">
-              <img src="@/assets/image/logo.png" alt />
-            </div>
-          </div>
-          <div class="nav-container">
-            <div class="nav-content float-clearfix">
-              <div class="nav-wrap">
-                <ul class="nav nav-pills e-nav-pills">
-                  <li @click="MakeUp">彩妆</li>
-                  <li>香水</li>
-                  <li>护肤</li>
-                  <li>尊享礼盒</li>
-                  <li>618年中盛典</li>
-                  <li>会员中心</li>
-                  <li>私人定制服务</li>
-                </ul>
-              </div>
-              <div class="functional-area">
-                <div class="form-input none-sm">
-                  <div>
-                    <input type="text" class="searchKeywords-pre" placeholder="请输入" />
-                    <span class="header-search-btn e-header-search">
-                      <i class="icon icon-header-search"></i>
-                    </span>
-                  </div>
-                </div>
-                <ul>
+                <ol>
                   <li class="header-subscribe-email e-header-subscribe-email is-subscribe">
                     <span>订阅电子杂志</span>
                   </li>
@@ -59,7 +31,117 @@
                     <span class="event-shoppingcart-count">购物袋-0件商品</span>
                     <i class="icon icon-down-arrow"></i>
                   </li>
+                </ol>
+              </div>
+            </div>
+            <div class="login">
+              <img src="@/assets/image/logo.png" alt />
+            </div>
+          </div>
+          <div class="nav-container">
+            <div class="nav-content float-clearfix" ref="nav">
+              <div class="nav-wrap">
+                <ul class="nav nav-pills e-nav-pills">
+                  <li @click="MakeUp" @mouseover="mouseOver" @mouseleave="mouseLeave">
+                    <span>彩妆</span>
+                    <div ref="erji" class="erji">
+                      <ul>
+                        <li>
+                          <span>唇部</span>
+                          <div class="sanji">
+                            <ul>
+                              <li>
+                                <span>唇膏</span>
+                              </li>
+                              <li>
+                                <span>唇釉</span>
+                              </li>
+                              <li>
+                                <span>唇颊霜</span>
+                              </li>
+                            </ul>
+                          </div>
+                        </li>
+                        <li>
+                          <span>面部</span>
+                          <div class="sanji">
+                            <ul>
+                              <li>
+                                <span>唇膏</span>
+                              </li>
+                              <li>
+                                <span>唇釉</span>
+                              </li>
+                              <li>
+                                <span>唇颊霜</span>
+                              </li>
+                            </ul>
+                          </div>
+                        </li>
+                        <li>
+                          <span>眼部</span>
+                          <div class="sanji">
+                            <ul>
+                              <li>
+                                <span>唇膏</span>
+                              </li>
+                              <li>
+                                <span>唇釉</span>
+                              </li>
+                              <li>
+                                <span>唇颊霜</span>
+                              </li>
+                            </ul>
+                          </div>
+                        </li>
+                        <li>
+                          <span>套装工具</span>
+                        </li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li>
+                    <span>香水</span>
+                  </li>
+                  <li>
+                    <span>护肤</span>
+                  </li>
+                  <li>
+                    <span>尊享礼盒</span>
+                  </li>
+                  <li>
+                    <span>618年中盛典</span>
+                  </li>
+                  <li>
+                    <span>会员中心</span>
+                  </li>
+                  <li>
+                    <span>私人定制服务</span>
+                  </li>
                 </ul>
+              </div>
+              <div class="functional-area">
+                <div class="form-input none-sm">
+                  <div>
+                    <input type="text" class="searchKeywords-pre" placeholder="请输入" />
+                    <span class="header-search-btn e-header-search">
+                      <i class="icon icon-header-search"></i>
+                    </span>
+                  </div>
+                </div>
+                <!-- <ul>
+                  <li class="header-subscribe-email e-header-subscribe-email is-subscribe">
+                    <span>订阅电子杂志</span>
+                  </li>
+                  <li class="header-login e-header-login" @click="login">
+                    <span>登录与注册</span>
+                  </li>
+                  <li class="header-shopping-bag e-header-shopping-bag is-empty">
+                    <i class="icon icon-header-shopping-bag"></i>
+                    <span class="event-shoppingcart-count">购物袋-0件商品</span>
+                    <i class="icon icon-down-arrow"></i>
+                  </li>
+                </ul>-->
               </div>
             </div>
           </div>
@@ -72,7 +154,14 @@
 export default {
   name: "Header",
   data() {
-    return {};
+    return {
+      active: "",
+      scrollPos: ""
+    };
+  },
+  whatch: {},
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
   },
   methods: {
     login() {
@@ -80,7 +169,42 @@ export default {
     },
     MakeUp() {
       this.$router.push("/MakeUp");
+    },
+    //鼠标移入
+    mouseOver() {
+      // 操作dom 获取标签改变其样式
+      var erji = this.$refs.erji;
+      erji.style.display = "block";
+    },
+    // 移出
+    mouseLeave() {
+      this.active = "";
+      var erji = this.$refs.erji;
+      erji.style.display = "none";
+    },
+    handleScroll() {
+      // console.log(document.documentElement.scrollTop);
+      if (document.documentElement.scrollTop > 100) {
+        this.$refs.nav.style.position = "fixed";
+        this.$refs.nav.style.top = "0";
+        this.$refs.nav.style.left = "0";
+        this.$refs.nav.style.background = "#fff";
+        this.$refs.nav.style.minWidth = "100%";
+        this.$refs.nav.style.width = "100%";
+      } else {
+        this.$refs.nav.style.position = "static";
+        this.$refs.nav.style.minWidth = "auto";
+        this.$refs.nav.style.width = "auto";
+      }
     }
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.handleScroll);
   }
 };
 </script>
+<style lang="scss" scoped>
+.el-main {
+  overflow: unset;
+}
+</style>
--- myDemo/src/views/home/home.vue
+++ myDemo/src/views/home/home.vue
@@ -7,28 +7,28 @@
           <div class="article por a1">
             <img src="@/assets/image/gw.jpg" alt />
           </div>
-          <div class="article por a1">
+          <div class="article por a2">
             <img src="@/assets/image/PC_02.jpg" alt />
           </div>
-          <div class="article por a1">
+          <div class="article por a3">
             <img src="@/assets/image/PC_03.jpg" alt />
           </div>
-          <div class="article por a1">
+          <div class="article por a4">
             <img src="@/assets/image/PC_04.jpg" alt />
           </div>
-          <div class="article por a1">
+          <div class="article por a4">
             <img src="@/assets/image/PC_05.jpg" alt />
           </div>
-          <div class="article por a1">
+          <div class="article por a4">
             <img src="@/assets/image/PC_06.jpg" alt />
           </div>
-          <div class="article por a1">
+          <div class="article por a7">
             <img src="@/assets/image/PC_07.jpg" alt />
           </div>
-          <div class="article por a1">
+          <div class="article por a4">
             <img src="@/assets/image/PC_08.jpg" alt />
           </div>
-          <div class="article por a1">
+          <div class="article por a9">
             <img src="@/assets/image/PC_20.jpg" alt />
           </div>
         </div>
@@ -59,9 +59,11 @@ export default {
 .pc-part .article {
   padding: 0;
 }
+.pc-part .a4 {
+  cursor: pointer;
+}
 .article img {
   width: 100%;
   display: block;
-  cursor: pointer;
 }
 </style>
